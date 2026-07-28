3b70ddd28b206e4d090fceb8860048714b3b95cc
Lance Shaw <lanceshaw25@gmail.com>
2026-07-28 17:13:13 -0600

    page component

---
+++ src/Page.jsx
@@ -0,0 +1,13 @@
+function Page () {
+    return (
+        <ul>
+            <li>Number 1</li>
+            <li>number 2</li>
+            <li>number 3</li>
+            <li>number 4</li>
+            <li>number 5</li>
+        </ul>
+    )
+}
+
+export default Page
--- src/index.jsx
+++ src/index.jsx
@@ -1,32 +1,40 @@
-/*
-Challenge: Starting from scratch, build and render the
-HTML for our section project. Check the Google slide for
-what you're trying to build.
-
-We'll be adding more styling to it later.
-
-Hints:
-* The React logo is a file in the project tree, so you can
-  access it by using `src="react-logo.png"` in your image
-  element
-* You can also set the `width` attribute of the image element
-  just like in HTML. In the slide, I have it set to 40px
-*/
-
-import {createRoot} from "react-dom/client"
-import reactLogo from "./assets/react.svg";
+import { createRoot } from "react-dom/client"
 const root = createRoot(document.getElementById("root"))
-root.render(
-    <main>
-        <img src={reactLogo} className="framework" alt="React logo"/>
 
-        <h1>Fun facts about React</h1>
-        <ul>
-            <li>Was first released in 2013</li>
-            <li>Was originally created by Jordan Walke</li>
-            <li>Has well over 100K stars on GitHub</li>
-            <li>Is maintained by Meta</li>
-            <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
-    </main>
+import Page from "./Page.jsx";
+import reactLogo from "./assets/react.svg";
+
+/**
+ Challenge:
+
+ Part 2:
+ - Add a `<header>` element with an `<img />` element with the image of the
+ React logo inside (src="react-logo.png") and make sure to set the
+ width to something more manageable so it doesn't take up the whole screen.
+ Also, as always, you should include some alt text on the image.
+ - Add an `<h1>` with some text describing the page. (E.g. "Reasons
+ I'm excited to learn React"). Place it above the ordered list, then wrap
+ the `<h1>` and `<ol>` inside a `<main>` element to keep our semantic
+ structure flowing well.
+ - Add a `<footer>` after the list that says:
+ "© 20xx <last name here> development. All rights reserved."
+ */
+
+
+root.render(
+    <>
+        <header id="header">
+            <img src={reactLogo} className="framework" alt="React logo" />
+        </header>
+        <main id="main">
+            <h1>Reasons I'm excited to learn React</h1>
+            <Page />
+        </main>
+        <footer id="footer">
+            <small>© 2026 Shaw development.  You can have it if you want."</small>
+        </footer>
+
+    </>
+
 )
+
